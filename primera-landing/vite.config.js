f8e1e5ee37dd259b0b5608056e9cf2935344ee40
Nicolas Riveira <riveiranicolas@gmail.com>
2025-10-05 21:15:42 -0300

    Cambios menores

--- primera-landing/vite.config.js
+++ primera-landing/vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/primer-proyecto-React/',
+  build: { outDir: 'docs' },
   plugins: [react()],
 })
