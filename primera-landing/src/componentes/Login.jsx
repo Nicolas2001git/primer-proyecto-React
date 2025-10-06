da07b7f5ba0a866fdf49836807bbfc86a5124d82
Nicolas Riveira <riveiranicolas@gmail.com>
2025-10-06 07:42:27 -0300

    Cambios menores

--- primera-landing/src/componentes/Login.jsx
+++ primera-landing/src/componentes/Login.jsx
@@ -3,7 +3,7 @@ function Login({ onLogin }) {
   const [name, setName] = useState("");
   return (
     <div className="contenedor-login">
-      <input className="entrada-nombre" type="text" placeholder="Pon tu nombre para entrar a Mordor" value={name} onChange={(event) => setName(event.target.value)}/>
+      <input className="entrada-nombre" type="text" placeholder="Pon tu nombre para entrar a la tierra Media" value={name} onChange={(event) => setName(event.target.value)}/>
       <button className="boton-entrar" onClick={() => onLogin(name.trim())} disabled={name.trim() === ""}>
         Entrar
       </button>
