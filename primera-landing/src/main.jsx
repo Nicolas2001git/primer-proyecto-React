72f55c3db293ee3c5b0e87d6bed4ac02fd4ff2d7
Nicolas Riveira <riveiranicolas@gmail.com>
2025-10-23 14:08:42 -0300

    cambios menores

--- primera-landing/src/main.jsx
+++ primera-landing/src/main.jsx
@@ -3,11 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import 'antd/dist/reset.css'; 
+import 'antd/dist/reset.css'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>       
+    <BrowserRouter basename="/primer-proyecto-React">       
       <App />
     </BrowserRouter>
-  </StrictMode>,
+  </StrictMode>
 )
